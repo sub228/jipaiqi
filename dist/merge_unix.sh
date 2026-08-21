39f505f2ee79ca18084c860eeb3f19a906b2faf1
sub228 <sub228@users.noreply.github.com>
2026-08-21 14:56:10 +0000

    feat: 复刻斗地主记牌器
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

---
+++ dist/merge_unix.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Linux / macOS / 安卓 Termux
+cat JiPaiQi-DouZero-debug-v2.1.apk.z01 JiPaiQi-DouZero-debug-v2.1.apk.z02 JiPaiQi-DouZero-debug-v2.1.apk.z03 JiPaiQi-DouZero-debug-v2.1.apk.zip > JiPaiQi-DouZero-debug-v2.1.apk
+md5sum JiPaiQi-DouZero-debug-v2.1.apk
+echo "正确 MD5 = c3b50012b4e258af835c73f54af078f1"
